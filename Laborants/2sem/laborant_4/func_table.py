3a4204fa081f15891757dda45211425131455e9b
Pon <is02n15ds@bk.ru>
2025-01-20 08:35:40 +0900

    Update func_table.py

--- Laborants/2sem/laborant_4/func_table.py
+++ Laborants/2sem/laborant_4/func_table.py
@@ -1,7 +1,10 @@
 def func_table(f, x_max, y_max):
-    for y in range(y_max + 1):
+    rows = []
+    for x in range(x_max + 1):
         row = []
-        for x in range(x_max + 1):
+        for y in range(y_max + 1):
             result = eval(f)
             row.append(str(result))
+        rows.append(row)
+    for row in rows:
         print('\t'.join(row))
