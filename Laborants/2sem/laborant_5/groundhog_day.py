f5a4b5b1d8f607323bd88adf8ffc64c62fd32698
Pon <is02n15ds@bk.ru>
2025-01-23 10:23:55 +0900

    Update groundhog_day.py

--- Laborants/2sem/laborant_5/groundhog_day.py
+++ Laborants/2sem/laborant_5/groundhog_day.py
@@ -1,15 +1,9 @@
 def groundhog_day(strings):
     for i in range(1, len(strings)):
-        current_string = strings[i]
-        previous_string = strings[i - 1]
-        if len(current_string) != len(previous_string):
-            continue
+        current_string, previous_string = strings[i], strings[i - 1]
         differences = 0
-        length = max(len(current_string), len(previous_string))
-        for j in range(length):
-            a = current_string[j] if j < len(current_string) else None
-            b = previous_string[j] if j < len(previous_string) else None
-            if a != b:
+        for j in range(len(current_string)):
+            if current_string[j] != previous_string[j]:
                 differences += 1
         if differences > 2:
             differing_indices = [index for index in range(len(current_string)) if
