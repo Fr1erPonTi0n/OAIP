565279a1c12807882aafff31c9ca9ce3a971bcf9
Pon <is02n15ds@bk.ru>
2025-02-03 16:52:41 +0900

    Update laborant_1.py

--- NM/Laborants/laborant_1.py
+++ NM/Laborants/laborant_1.py
@@ -2,6 +2,7 @@ import math
 
 
 def approximate_value(x, flag):
+    """Функция, которая вычисляет приближенного значения суммы ряда Тейлора."""
     s, a, n = 1, x, 1
     if flag:    # УСТОЙЧИВЫЙ АЛГОРИТМ (НЕ СДЕЛАН)
         while s + a != s:
@@ -20,14 +21,14 @@ nums = [-250, -200, -150, -100, -50, 0, 50, 100, 150, 200, 250]
 
 print('НЕУСТОЙЧИВЫЙ АЛГОРИТМ')
 for elem in nums:
-    num1 = approximate_value(elem, False)
-    num2 = math.exp(elem)
-    num3 = abs(num2 - num1) / num2
+    num1 = approximate_value(elem, False)     # Число с написанной нашей функции.
+    num2 = math.exp(elem)                     # Число из библиотеки math, функция exp.
+    num3 = abs(num2 - num1) / num2            # Относительная погрешность
     print(f"{f'x = {elem}':<15} {f's = {num1}':<30} {f'math_exp = {num2}':<40} {f'Δ = {num3}':<35}")
+    
 print('УСТОЙЧИВЫЙ АЛГОРИТМ')
 for elem in nums:
-    num1 = approximate_value(elem, True)
-    num2 = math.exp(elem)
-    num3 = abs(num2 - num1) / num2
+    num1 = approximate_value(elem, False)     # Число с написанной нашей функции.
+    num2 = math.exp(elem)                     # Число из библиотеки math, функция exp.
+    num3 = abs(num2 - num1) / num2            # Относительная погрешность
     print(f"{f'x = {elem}':<15} {f's = {num1}':<30} {f'math_exp = {num2}':<40} {f'Δ = {num3}':<35}")
-
