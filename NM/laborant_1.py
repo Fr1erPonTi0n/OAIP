affaa73d435951e7d4068581fa4d091e2a23263a
Pon <is02n15ds@bk.ru>
2025-02-03 09:18:53 +0900

    Update laborant_1.py

--- NM/laborant_1.py
+++ NM/laborant_1.py
@@ -18,13 +18,13 @@ print('НЕУСТОЙЧИВЫЙ АЛГОРИТМ')
 for elem in nums:
     num1 = approximate_value(elem)
     num2 = math.exp(elem)
-    num3 = abs(approximate_value(elem) - math.exp(elem)) / approximate_value(elem)
+    num3 = abs(num1 - num2) / num1
     print(f"x = {elem}\t\ts = {num1}\t\tmath_exp = {num2}\t\tΔ = {num3}")
 
 print('\nУСТОЙЧИВЫЙ АЛГОРИТМ')
 for elem in nums:
     num1 = approximate_value(elem)
     num2 = math.exp(elem)
-    num3 = abs(approximate_value(elem) - math.exp(elem)) / approximate_value(elem)
+    num3 = abs(num1 - num2) / num1
     print(f"x = {elem}\t\ts = {num1}\t\tmath_exp = {num2}\t\tΔ = {num3}")
 
